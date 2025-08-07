e010a017bc876262e55ea30a64298f40c00403a7
carolinejeppesen <caroline.t.jeppesen@gmail.com>
2025-08-07 11:07:51 +0200

    implement basic Todo List functionality with task addition

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
         <h1 class="text-4xl font-bold text-blue-300">Todo List</h1>
+        <input v-model="newTask" class="border border-gray-300 rounded px-4 py-2 mt-4 w-full" placeholder="Add a new todo" />
+        <button @click="addTask" class="bg-gray-300 px-4 py-2 mt-4">Add</button>
+        <ul>
+            <li v-for="(task, index) in tasks" :key="index">
+                {{ task.text }}
+            </li>
+        </ul>
     </div>
 </template>
+
+<script setup>
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push({ text: newTask.value.trim(), done: false, editing: false })
+    newTask.value = ''
+  }
+}
+</script>
