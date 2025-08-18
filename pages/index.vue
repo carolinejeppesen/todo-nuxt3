f8bfddd6ce1f4ab0ce939c60a5156303839d00f3
carolinejeppesen <caroline.t.jeppesen@gmail.com>
2025-08-18 10:18:36 +0200

    simplify index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gradient-to-br from-blue-50 via-white to-sky-50">
-    <div class="max-w-4xl mx-auto p-8">
-      <h1 class="text-3xl font-bold text-center mb-8">Index</h1>
-    </div>
-  </div>
+  <div class="min-h-screen bg-sky-50"></div>
 </template>
 
 <script setup></script>
