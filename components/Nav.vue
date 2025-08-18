3bbe81c816fd331a7de2b01bec4d393c1338a8a1
carolinejeppesen <caroline.t.jeppesen@gmail.com>
2025-08-18 10:18:19 +0200

    style: enhance Nav component layout and styling

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,11 @@
 <template>
-    <nav>
-        <NuxtLink to="/">Home </NuxtLink>
-        <NuxtLink to="/today">Today </NuxtLink>
-        <NuxtLink to="/tomorrow">Tomorrow </NuxtLink>
-    </nav>
+  <nav class="text-base gap-6 pt-8 flex justify-center bg-sky-50">
+    <NuxtLink :class="linkClass" to="/">Home </NuxtLink>
+    <NuxtLink :class="linkClass" to="/today">Today </NuxtLink>
+    <NuxtLink :class="linkClass" to="/tomorrow">Tomorrow </NuxtLink>
+  </nav>
 </template>
+
+<script setup>
+const linkClass = "bg-white p-3 rounded-xl shadow";
+</script>
