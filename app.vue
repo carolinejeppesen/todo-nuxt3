16356c64df576689fd9f4601bfe23d101aa311db
carolinejeppesen <caroline.t.jeppesen@gmail.com>
2025-08-18 11:40:41 +0200

    added: page transitions

--- app.vue
+++ app.vue
@@ -4,3 +4,19 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+html,
+body {
+  @apply bg-sky-50;
+}
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.2s ease;
+}
+.page-enter-from,
+.page-leave-active {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss'],
-})
+  modules: ["@nuxtjs/tailwindcss"],
+});
